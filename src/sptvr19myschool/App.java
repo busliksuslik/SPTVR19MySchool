c02d866aef75458065728aeb7fe09430eec04be4
Oleg <oleg.harlamov@ivkhk.ee>
2020-10-21 10:27:58 +0300

    first commit

---
+++ src/sptvr19myschool/App.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sptvr19myschool;
+
+/**
+ *
+ * @author user
+ */
+class App {
+
+    void run() {
+        System.out.println("Go fuck yourself");
+    }
+    
+}
